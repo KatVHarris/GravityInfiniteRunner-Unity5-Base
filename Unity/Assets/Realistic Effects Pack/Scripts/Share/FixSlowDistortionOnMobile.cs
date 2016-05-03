57473cdf5094af7ab71ccf518beb0ed4d9ef1f85
Katherine Harris <kvm.harris@gmail.com>
2016-05-03 08:39:59 -0700

    Clean Up phase one. Deleting loose scripts in Assets folder. Organizaing Prefabs.

--- Unity/Assets/Realistic Effects Pack/Scripts/Share/FixSlowDistortionOnMobile.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-public class FixSlowDistortionOnMobile: MonoBehaviour
-{
-  void OnRenderImage(RenderTexture src, RenderTexture dest)
-  {
-    Graphics.Blit(src, dest);
-  }
-}
